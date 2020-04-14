e4a0384f24f83f647276d8838479a1309fa952a6
Kelton <keltonsheridan@gmail.com>
2020-04-13 21:26:06 -0600

    can't figure out how to group_by/summarize

--- Sheridan-Reanalysis.R
+++ Sheridan-Reanalysis.R
@@ -1,11 +1,29 @@
 library(readxl)
+library(ggplot2)
+library(tidyselect)
+library(dplyr)
 #Reading in the data from table 1
 f <- "/Users/kelton/Desktop/data-reanalysis1.xlsx"
-d <- read_excel(f, sheet = 1, col_names = TRUE)
-head(d)
+ceramic <- read_excel(f, sheet = 1, col_names = TRUE)
+head(ceramic)
+
+summary(ceramic)
+
+s <- summarize(
+  ceramic,
+  n_cases = n(),
+  avgSp = mean(Body_mass_female_mean, na.rm = TRUE),
+  avgM = mean(Body_mass_male_mean, na.rm = TRUE)
+)
+s
+
+names(ceramic)
+ggplot(data = ceramic, aes(y = Ca, x = Cs)) + geom_point()
+
+
 
 #Reading in the data from table 2
 f2 <- "/Users/kelton/Desktop/data-reanalysis2.xlsx"
-d2 <- read_excel(f, sheet = 1, col_names = TRUE)
-head(d2)
+ceramic2 <- read_excel(f, sheet = 1, col_names = TRUE)
+head(ceramic2)
 
